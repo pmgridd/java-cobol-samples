fcf2803ba6719cc9a5019876d69101055f8f5e46
pmgridd <pmarchenko@griddynamics.com>
2025-10-20 14:47:30 +0200

    Created main Spring Boot application file

---
+++ java/src/main/java/com/example/javacobolsamples/JavaCobolSamplesApplication.java
@@ -0,0 +1,12 @@
+package com.example.javacobolsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCobolSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCobolSamplesApplication.class, args);
+    }
+}
